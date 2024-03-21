869f06ec2a0100cf9adb058b98d351c589108d47
deadlykittengs <ryan270win@gmail.com>
2024-03-21 12:48:15 +0200

    calc html built li declared

---
+++ plugin.js
@@ -0,0 +1,49 @@
+
+let a
+let b
+let c
+let display
+
+var one = document.getElementById("one")
+var two = document.getElementById("two")
+var three = document.getElementById("three")
+var four = document.getElementById("four")
+var five = document.getElementById("five")
+var six = document.getElementById("six")
+var seven = document.getElementById("seven")
+var eight = document.getElementById("eight")
+var nine = document.getElementById("nine")
+var zero = document.getElementById("zero")
+var add = document.getElementById("add")
+var subtract = document.getElementById("subtract")
+var multiply = document.getElementById("multiply")
+var devide = document.getElementById("divide")
+var clear = document.getElementById("clear")
+var equals = document.getElementById("equals")
+
+
+one.addEventListener('click', e => { console.log(1) } )
+
+
+
+function add (a,b){
+    a + b == c
+    return c
+}
+function subtract (a,b){
+ a - b == c
+ return c
+}
+ 
+function  multiply (a,b){
+    a * b == c
+    return c
+
+}
+function devide (a,b){
+    a % b == c
+    return c
+}
+
+
+console.log ()
